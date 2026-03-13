9fe2d10943da567b362ad92e1c1e7132830a4a5e
Mohamed H. Hussein <Mohamedhossam7077@gmail.com>
2026-03-13 15:30:11 +0200

    Update 01_download_reference.sh

--- African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/01_download_reference.sh
+++ African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/01_download_reference.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+echo "Downloading reference genome"
+
+mkdir -p workshop
+cd workshop
+
+datasets download genome accession GCF_000001735.4 --include genome,gff3
+
+unzip ncbi_dataset.zip
+
+echo "Reference genome downloaded and extracted"
