a497992f97fd7a5634a5fd0230f36fd917c4872d
Mohamed H. Hussein <Mohamedhossam7077@gmail.com>
2026-03-13 15:33:10 +0200

    Update 06_dedup_base_recal.sh

--- African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/06_dedup_base_recal.sh
+++ African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/06_dedup_base_recal.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+echo "Running duplicate marking"
+
+cd workshop
+
+picard MarkDuplicates \
+I=ERR11436045_sorted_trimmed_ref_aligned.bam \
+O=ERR11436045_dedup.bam \
+M=dup_metrics.txt
+
+samtools index ERR11436045_dedup.bam
+
+echo "Deduplication completed"
