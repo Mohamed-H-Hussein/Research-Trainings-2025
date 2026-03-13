73099610dabe3faaef56e62bab4e5afebce8d443
Mohamed H. Hussein <Mohamedhossam7077@gmail.com>
2026-03-13 15:30:50 +0200

    Update 02_index_reference.sh

--- African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/02_index_reference.sh
+++ African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/02_index_reference.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+echo "Indexing reference genome"
+
+cd workshop
+
+mkdir -p index
+
+mv ncbi_dataset/data/GCF_000001735.4/GCF_000001735.4_TAIR10.1_genomic.fna index/
+
+bwa index index/GCF_000001735.4_TAIR10.1_genomic.fna
+
+samtools faidx index/GCF_000001735.4_TAIR10.1_genomic.fna
+
+echo "Reference indexing completed"
