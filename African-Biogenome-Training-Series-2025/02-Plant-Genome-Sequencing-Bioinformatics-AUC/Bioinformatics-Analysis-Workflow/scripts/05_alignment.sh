4b85d030cd0827626949d4555882a9caf877f837
Mohamed H. Hussein <Mohamedhossam7077@gmail.com>
2026-03-13 15:32:33 +0200

    Update 05_alignment.sh

--- African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/05_alignment.sh
+++ African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/05_alignment.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+echo "Running alignment"
+
+cd workshop
+
+bwa mem -t 4 index/GCF_000001735.4_TAIR10.1_genomic.fna \
+sra_data/ERR11436045_1.trimmed.fastq \
+sra_data/ERR11436045_2.trimmed.fastq \
+> ERR11436045_trimmed_ref_aligned.sam
+
+samtools view -bS -@ 4 ERR11436045_trimmed_ref_aligned.sam \
+> ERR11436045_trimmed_ref_aligned.bam
+
+samtools sort -@4 ERR11436045_trimmed_ref_aligned.bam \
+-o ERR11436045_sorted_trimmed_ref_aligned.bam
+
+samtools index ERR11436045_sorted_trimmed_ref_aligned.bam
+
+echo "Alignment completed"
