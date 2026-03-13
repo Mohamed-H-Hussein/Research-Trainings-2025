52a12683fc91171295afb1aa2d7c4cbcd7df87f6
Mohamed H. Hussein <Mohamedhossam7077@gmail.com>
2026-03-13 15:31:24 +0200

    Update 03_download_reads.sh

--- African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/03_download_reads.sh
+++ African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/03_download_reads.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+echo "Downloading sequencing reads"
+
+cd workshop
+
+mkdir -p sra_data
+cd sra_data
+
+prefetch ERR11436045
+
+fasterq-dump ERR11436045 --split-files
+
+echo "Reads downloaded and converted to FASTQ"
