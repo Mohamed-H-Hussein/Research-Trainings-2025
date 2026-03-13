76d56c7cab68da218288f17155853d279b377fe7
Mohamed H. Hussein <Mohamedhossam7077@gmail.com>
2026-03-13 15:33:41 +0200

    Update 07_variant_calling.sh

--- African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/07_variant_calling.sh
+++ African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/07_variant_calling.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+echo "Running variant calling"
+
+cd workshop
+
+bcftools mpileup \
+-f index/GCF_000001735.4_TAIR10.1_genomic.fna \
+--threads 4 \
+ERR11436045_dedup.bam \
+-Ou | bcftools call -mv -Oz -o variants.vcf.gz
+
+bcftools index variants.vcf.gz
+
+bcftools filter -i 'QUAL>30' variants.vcf.gz \
+-Oz -o filtered_variants.vcf.gz
+
+bcftools view -v snps filtered_variants.vcf.gz \
+-Oz -o snps.vcf.gz
+
+echo "Variant calling finished"
