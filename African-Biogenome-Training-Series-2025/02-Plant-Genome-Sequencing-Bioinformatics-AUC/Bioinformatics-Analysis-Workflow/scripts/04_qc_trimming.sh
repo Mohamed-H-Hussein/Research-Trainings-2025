60da2b0d7d23f5822cccae599b31119b65af5e9e
Mohamed H. Hussein <Mohamedhossam7077@gmail.com>
2026-03-13 15:31:58 +0200

    Update 04_qc_trimming.sh

--- African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/04_qc_trimming.sh
+++ African-Biogenome-Training-Series-2025/02-Plant-Genome-Sequencing-Bioinformatics-AUC/Bioinformatics-Analysis-Workflow/scripts/04_qc_trimming.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+echo "Running quality control and trimming"
+
+cd workshop
+
+mkdir -p fastqc_output
+
+fastqc -o fastqc_output -t 4 sra_data/ERR11436045_1.fastq sra_data/ERR11436045_2.fastq
+
+fastp \
+-i sra_data/ERR11436045_1.fastq \
+-I sra_data/ERR11436045_2.fastq \
+-o sra_data/ERR11436045_1.trimmed.fastq \
+-O sra_data/ERR11436045_2.trimmed.fastq \
+-l 100 \
+-w 4
+
+fastqc -o fastqc_output -t 4 \
+sra_data/ERR11436045_1.trimmed.fastq \
+sra_data/ERR11436045_2.trimmed.fastq
+
+echo "QC and trimming completed"
